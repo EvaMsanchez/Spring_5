129f2045a11518cf76850a0092e469b25418a911
EvaMsanchez <eva_crm20@hotmail.com>
2024-10-17 11:27:13 +0200

    Personalizando error 404

--- src/main/java/com/eva/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/eva/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.eva.curso.springboot.error.springboot_error.models.Error;
 
@@ -24,5 +25,21 @@ public class HandlerExceptionController
         
         return ResponseEntity.internalServerError().body(error);
         //return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    }
+
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> notFoundEx(NoHandlerFoundException e)
+    {
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Api rest no encontrado");
+        error.setMessage(e.getMessage());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        
+        return ResponseEntity.status(404).body(error);
+        // return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
+
+        // Hay que añadir una configuración en el application.properties para que se pueda mostrar el mensaje personalizado en el error 404
     }
 }
